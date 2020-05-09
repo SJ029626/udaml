27760af1bf5bb51560d682ea087006382ad7f704
Sanyam Jain <14850141+SJ029626@users.noreply.github.com>
2020-05-10 01:37:00 +0530

    feat: new project added

---
+++ run_kubernetes.sh
@@ -0,0 +1,22 @@
+ 
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath="sanyamj/udaml"
+tag_name="mldocker"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run $tag_name --image=$dockerpath --port=80 --labels app=$tag_name
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward $tag_name 8000:80
